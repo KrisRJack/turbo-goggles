b0a5e81a7969cd0b8c1a629ee023139c74bb4eda
Kris Jackson <kristopherrjackson@gmail.com>
2022-05-27 12:25:29 -0400

    Added categories and conditions

---
+++ Exchange-iOS/Shared/Globals/Categories.swift
@@ -0,0 +1,39 @@
+//
+//  Categories.swift
+//  Exchange-iOS
+//
+//  Created by Kristopher Jackson on 5/27/22.
+//
+
+import Foundation
+
+let categories = [
+    
+    "Women's Clothing",
+    "Women's Accessories",
+    "Men's Clothing",
+    "Men's Accessories",
+    "Antiques",
+    "Appliances",
+    "Art","Beauty",
+    "Books",
+    "Electronics",
+    "Crafts",
+    "Entertainment",
+    "Food",
+    "Gaming",
+    "Health",
+    "Home",
+    "Jewelry",
+    "Music",
+    "Pet",
+    "Specialty Services",
+    "Sporting Goods",
+    "Supplies",
+    "Textbooks",
+    "Tickets/Experiences",
+    "Toys",
+    "Travel",
+    "Other",
+    
+]
---
+++ Exchange-iOS/Shared/Globals/Conditions.swift
@@ -0,0 +1,17 @@
+//
+//  Conditions.swift
+//  Exchange-iOS
+//
+//  Created by Kristopher Jackson on 5/27/22.
+//
+
+import Foundation
+
+let conditions = [
+    
+    "Brand New",
+    "Used, Like New",
+    "Used, Good",
+    "Used, Poor",
+    
+]
