f0ce112b11e2edf00a60f9f440f63fcea92b02d7
Kris Jackson <kristopherrjackson@gmail.com>
2022-05-27 10:43:30 -0400

    Updated table view header spacing

--- Exchange-iOS/Features/Listing/ViewControllers/NewListingViewController.swift
+++ Exchange-iOS/Features/Listing/ViewControllers/NewListingViewController.swift
@@ -33,6 +33,10 @@ final class NewListingViewController: UITableViewController {
         super.viewDidLoad()
         setUpNavigationBar()
         configureTableViewHeader()
+        tableView.allowsSelection = false
+        tableView.separatorStyle = .none
+        tableView.register(MaterialTextViewCell.self, forCellReuseIdentifier: MaterialTextViewCell.reuseIdentifier)
+        tableView.register(MaterialTextFieldCell.self, forCellReuseIdentifier: MaterialTextFieldCell.reuseIdentifier)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -72,9 +76,40 @@ final class NewListingViewController: UITableViewController {
     private func configureTableViewHeader() {
         tableView.tableHeaderView = UIView()
         tableView.tableHeaderView?.addSubviews(listingPhotosViewController.view)
-        tableView.tableHeaderView?.frame.size.height = 220
+        tableView.tableHeaderView?.frame.size.height = 244
         addChild(listingPhotosViewController)
         listingPhotosViewController.didMove(toParent: self)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return form.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let data = form[indexPath.row]
+        
+        switch data.fieldType {
+        case .textfield:
+            let cell = tableView.dequeueReusableCell(withIdentifier: MaterialTextFieldCell.reuseIdentifier, for: indexPath)
+            (cell as? MaterialTextFieldCell)?.title = data.title
+            (cell as? MaterialTextFieldCell)?.placeholder = data.placeholder
+            (cell as? MaterialTextFieldCell)?.subtext = data.subtitle
+            (cell as? MaterialTextFieldCell)?.setTextColor(to: .label)
+            (cell as? MaterialTextFieldCell)?.setPrimaryColor(to: .lightThemeColor)
+            (cell as? MaterialTextFieldCell)?.setSecondaryColor(to: .secondaryLabel)
+            return cell
+            
+        case .textView:
+            let cell = tableView.dequeueReusableCell(withIdentifier: MaterialTextViewCell.reuseIdentifier, for: indexPath)
+            (cell as? MaterialTextViewCell)?.title = data.title
+            (cell as? MaterialTextViewCell)?.placeholder = data.placeholder
+            (cell as? MaterialTextViewCell)?.subtext = data.subtitle
+            (cell as? MaterialTextViewCell)?.setTextColor(to: .label)
+            (cell as? MaterialTextViewCell)?.setPrimaryColor(to: .lightThemeColor)
+            (cell as? MaterialTextViewCell)?.setSecondaryColor(to: .secondaryLabel)
+            return cell
+        }
+        
     }
     
 }
@@ -91,4 +126,61 @@ extension NewListingViewController: ListingPhotosDelegate {
         navigationDelegate?.goToEditImage(from: self, at: indexPath, with: images)
     }
     
+}
+
+// MARK: - Form
+
+extension NewListingViewController {
+    
+    enum FieldType {
+        case textfield
+        case textView
+    }
+    
+    struct Form {
+        let title: String
+        let subtitle: String?
+        let placeholder: String
+        let fieldType: FieldType
+    }
+    
+    var form: [Form] {
+        [
+            Form(
+                title: NSLocalizedString("LISTING_TITLE_HEADER", comment: "Header"),
+                subtitle: nil,
+                placeholder: NSLocalizedString("LISTING_TITLE_PLACEHOLDER", comment: "Placeholder"),
+                fieldType: .textfield
+            ),
+            
+            Form(
+                title: NSLocalizedString("LISTING_PRICE_HEADER", comment: "Header"),
+                subtitle: nil,
+                placeholder: NSLocalizedString("LISTING_PRICE_PLACEHOLDER", comment: "Placeholder"),
+                fieldType: .textfield
+            ),
+            
+            Form(
+                title: NSLocalizedString("LISTING_DESCRIPTION_HEADER", comment: "Header"),
+                subtitle: NSLocalizedString("LISTING_DESCRIPTION_TEXT", comment: "General"),
+                placeholder: NSLocalizedString("LISTING_DESCRIPTION_PLACEHOLDER", comment: "Placeholder"),
+                fieldType: .textView
+            ),
+            
+            Form(
+                title: NSLocalizedString("LISTING_SIZE_HEADER", comment: "Header"),
+                subtitle: NSLocalizedString("LISTING_SIZE_TEXT", comment: "Placeholder"),
+                placeholder: NSLocalizedString("LISTING_SIZE_PLACEHOLDER", comment: "Placeholder"),
+                fieldType: .textfield
+            ),
+            
+            Form(
+                title: NSLocalizedString("LISTING_TAG_HEADER", comment: "Header"),
+                subtitle: NSLocalizedString("LISTING_TAG_TEXT", comment: "Placeholder"),
+                placeholder: NSLocalizedString("LISTING_TAG_PLACEHOLDER", comment: "Placeholder"),
+                fieldType: .textfield
+            ),
+            
+        ]
+    }
 }
