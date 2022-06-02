1a0ed6ce814c4e93830c708ff034efd848cd37a3
Kris Jackson <kristopherrjackson@gmail.com>
2022-06-02 06:11:46 -0400

    Added Vibration enum

---
+++ Exchange-iOS/Shared/Models/Vibration.swift
@@ -0,0 +1,49 @@
+//
+//  Vibration.swift
+//  Exchange-iOS
+//
+//  Created by Kristopher Jackson on 6/2/22.
+//
+
+import UIKit
+import AudioToolbox
+
+enum Vibration {
+    
+    case error
+    case success
+    case warning
+    case light
+    case medium
+    case heavy
+    case soft
+    case rigid
+    case selection
+    case oldSchool
+    
+    static func vibrate(type: Vibration) {
+        switch type {
+        case .error:
+            UINotificationFeedbackGenerator().notificationOccurred(.error)
+        case .success:
+            UINotificationFeedbackGenerator().notificationOccurred(.success)
+        case .warning:
+            UINotificationFeedbackGenerator().notificationOccurred(.warning)
+        case .light:
+            UIImpactFeedbackGenerator(style: .light).impactOccurred()
+        case .medium:
+            UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+        case .heavy:
+            UIImpactFeedbackGenerator(style: .heavy).impactOccurred()
+        case .soft:
+            UIImpactFeedbackGenerator(style: .soft).impactOccurred()
+        case .rigid:
+            UIImpactFeedbackGenerator(style: .rigid).impactOccurred()
+        case .selection:
+            UISelectionFeedbackGenerator().selectionChanged()
+        case .oldSchool:
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+        }
+    }
+    
+}
