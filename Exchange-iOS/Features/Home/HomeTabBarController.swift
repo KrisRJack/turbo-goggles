e630244deddf57599d326745d76e9b177cdad862
Kris Jackson <kristopherrjackson@gmail.com>
2022-06-18 07:35:15 -0400

    Changed tab bar background color

--- Exchange-iOS/Features/Home/HomeTabBarController.swift
+++ Exchange-iOS/Features/Home/HomeTabBarController.swift
@@ -32,7 +32,6 @@ class HomeTabBarController: UITabBarController {
     
     private func setUpTabBarAppearance() {
         let appearance = tabBar.standardAppearance
-        appearance.backgroundColor = .white
         appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white]
         appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.white]
         appearance.stackedLayoutAppearance.normal.iconColor = .black
@@ -41,6 +40,7 @@ class HomeTabBarController: UITabBarController {
         tabBar.layer.borderWidth = 0.2
         tabBar.layer.borderColor = UIColor.separator.cgColor
         tabBar.clipsToBounds = true
+        tabBar.backgroundColor = .systemBackground
     }
     
 }
